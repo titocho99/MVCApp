608b62cfa81aea8d2da8e929b4fcb87d4aad2951
Arturo Segoviano Valdes <Arturo_85_60@hotmail.com>
2023-11-19 19:34:38 -0600

    Agregar archivos de proyecto.

---
+++ MVCApp/Models/SeedData.cs
@@ -0,0 +1,62 @@
+﻿using Humanizer.Localisation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MVCApp.Data;
+using System;
+using System.Linq;
+
+namespace MVCApp.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MVCAppContext(
+            serviceProvider.GetRequiredService<DbContextOptions<MVCAppContext>>()))
+
+            {
+                if (context.Movie.Any())
+                {
+                    //look for any movies
+                    return; //DB has been
+                }
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Avenger 1",
+                        RealeseDate = DateTime.Parse("1989-2-12") ,
+                        Genre = "Fiction",
+                        Price = 98.23M,
+                        Rating = "R"
+                    },
+                    new Movie
+                    {
+                        Title = "Avenger 2",
+                        RealeseDate = DateTime.Parse("2014-2-12") ,
+                        Genre = "Fiction",
+                        Price = 45.45M,
+                        Rating = "R"
+                    },
+                    new Movie
+                    {
+                        Title = "Avenger 3",
+                        RealeseDate = DateTime.Parse("2021-2-12"),
+                        Genre = "Fiction",
+                        Price = 145.47M,
+                        Rating = "R"
+                    },
+                    new Movie
+                    {
+                        Title = "Avenger 4",
+                        RealeseDate = DateTime.Parse("2014-2-12"),
+                        Genre = "Fiction",
+                        Price = 45.45M,
+                        Rating = "R"
+                    }
+                    );
+                context.SaveChanges();
+            }
+        }
+        
+    }
+}
---
+++ MVCApp/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MVCApp.Data;
+using MVCApp.Models;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MVCAppContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MVCAppContext") ?? throw new InvalidOperationException("Connection string 'MVCAppContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    SeedData.Initialize(services);
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
